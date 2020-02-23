5f437fb4a9d7001959dc1e04c791f72ae7906c26
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-23 10:58:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,25 @@ class Dog
   def name=(name)
     @name = name
   end
+  
   def name
     @name
   end
 end
 
-
-
 fido = Dog.new
 fido.name = "Fido"
 fido.name
+
+  def  breed = (breed)
+    @breed = (breed)
+  end
+
+  def breed 
+    @breed
+  end
+end
+
+snoopy = Dog.new
+snoopy.breed = "Beagle"
+snoopy.breed
