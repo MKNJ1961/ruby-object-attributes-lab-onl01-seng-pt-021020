558acc47f85acb23a6134857a03644499aaae493
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-23 14:49:11 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,13 @@
-class Person
-  def name=(person_name)
-    @name = person_name
+  class Person
+    def name=(person_name)
+      @name = person_name
+    end
+
+    def name
+      @name
+    end
   end
 
-  def name
-    @name
-  end
 
   beyonce = Person.new
   beyonce.name = "Beyonce"
